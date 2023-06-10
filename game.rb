4bcf968d8f33a9f9f1eb1f81a45c482503272132
mfm051 <mateusmatos051@gmail.com>
2023-06-10 13:04:39 -0300

    Update cross checker

--- game.rb
+++ game.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 # class Board # :nodoc:
-#   attr_reader :squares, :lines
+#   attr_reader :squares
 
 #   def initialize
 #     @squares = Array.new(3) { Array.new(3) }
-#     @lines = [squares[0], squares[1], squares[2]]
-#     @columns = lines.transpose
+#     @cross1 = [0, 1, 2].map { |i| squares[[i][i]] }
+#     @cross2 = [0, 1, 2].map { |i| squares[[i][i]] }
+#   end
+
+#   def lines
+#     [squares[0], squares[1], squares[2]]
+#   end
+
+#   def columns
+#     squares.transpose
+#   end
+
+#   def crosses
+#     [(0..2).map { |i| squares[[i][i]] }, [[0, 2], [1, 1], [2, 0]].map { |i, j| squares[i][j] }]
 #   end
 # end
 
@@ -47,17 +59,18 @@ class Game # :nodoc:
   end
 
   def check_cross_complete
-    crosses = [[squares[0][0], squares[1][1], squares[2][2]], [squares[2][0], squares[1][1], squares[0][2]]]
+    # crosses = [[squares[0][0], squares[1][1], squares[2][2]], [squares[2][0], squares[1][1], squares[0][2]]]
+    crosses = [(0..2).map { |i| squares[i][i] }, (0..2).map { |i| squares[i][2 - i] }]
     crosses.any? { |cross| cross.all?(player1) || cross.all?(player2) }
   end
 end
 
 # Tests
 game = Game.new
-game.pick_square(0, 0)
-game.pick_square(0, 1)
-game.pick_square(1, 0)
 game.pick_square(0, 2)
+game.pick_square(0, 1)
+game.pick_square(1, 1)
+game.pick_square(0, 0)
 game.pick_square(2, 0)
 p game.squares
 p game.check_line_complete
